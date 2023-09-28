28a72601d6f337ced1d26451b295377168459c21
Márcio J. Silva <marciojsilva159@gmail.com>
2023-09-28 13:34:15 -0300

    Public please

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Usuario} from '../shared/model/usuario';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primeiro-projeto-pw1';
+  titulo = 'First Project';
+  usuario: Usuario;
+  usuarios: Array<Usuario>;
+
+  constructor() {
+    this.usuario = new Usuario();
+    this.usuarios = new Array<Usuario>();
+  }
+
+  inserirUsuario(): void {
+    this.usuarios.push(this.usuario);
+    this.usuario = new Usuario();
+  }
+
 }
